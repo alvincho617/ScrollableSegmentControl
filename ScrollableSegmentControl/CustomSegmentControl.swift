e096921fa15231316eafb2a96163a5555c1eba0c
georgechurikov <georgechurikov@gmail.com>
2024-03-14 14:35:53 +0200

    inited

---
+++ ScrollableSegmentControl/CustomSegmentControl.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSegmentControl.swift
+//  ScrollableSegmentControl
+//
+//  Created by George Churikov on 14.03.2024.
+//
+
+import Foundation
---
+++ ScrollableSegmentControl/Resources/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  ScrollableSegmentControl
+//
+//  Created by George Churikov on 14.03.2024.
+//
+
+import Foundation
