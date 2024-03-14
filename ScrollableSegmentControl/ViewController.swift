05d2873561189900c9b3359ebac02f5aa1f38f46
georgechurikov <georgechurikov@gmail.com>
2024-03-14 14:31:10 +0200

    Initial Commit

---
+++ ScrollableSegmentControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollableSegmentControl
+//
+//  Created by George Churikov on 14.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
